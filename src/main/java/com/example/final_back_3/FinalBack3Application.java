d264da187e5cb31cf9c7642243b713c07b4a0248
Fes <Dimma046@list.com>
2023-11-19 21:24:31 +1000

    created empty project. Previous doesn`t work and idk why

---
+++ src/main/java/com/example/final_back_3/FinalBack3Application.java
@@ -0,0 +1,13 @@
+package com.example.final_back_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalBack3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalBack3Application.class, args);
+    }
+
+}
